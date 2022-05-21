cf8ca4ae52b45fa80b07b1d598503407339ea3a7
Andrew Ubag <jameandreu@gmail.com>
2022-05-22 00:25:59 +0800

    Fix backgrond position ule on desktop

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ let bgPosition = 0;
 content.addEventListener("scroll", function () {
 	let scroll = content.scrollTop;
 	// if (position < scroll) {
-	bgPosition = -(position * 0.03); //background move-up
+	bgPosition = -(position * 0.03); //this moves the background
 	console.log(bgPosition);
-	header.style.backgroundPosition = `left ${bgPosition}px`;
+	header.style.backgroundPosition = `center ${bgPosition}px`;
 	// } else {
 	// 	bgPosition = -position * 0.03; //background move-down
 	// 	console.log(bgPosition);
